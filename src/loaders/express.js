8597bc0929cf5e1c334ecd372379f204662f026b
elifgure <elif.gure121@gmail.com>
2025-08-26 16:05:36 +0300

    add middlewares

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,19 +1,37 @@
 // express app setup
-const express = require("express")
-const cors = require("cors")
-const morgan = require("morgan")
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const applySecurity = require("../middlewares/security");
+const { notFound, errorHandler } = require("../middlewares/error");
 
 function expressLoader() {
-    const app = express()
+  const app = express();
 
-    // middlewares
-    app.use(cors())
-    app.use(express.json())
-    app.use(morgan("dev"))
-    
-    // routes
+  // Güvenlik middleware'leri
+  applySecurity(app);
 
-    return app
+  // middlewares
+  app.use(cors());
+
+  // body parsing middleware
+  app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: true }));
+
+  // Logging
+  if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+  }
+
+  // routes
+
+  //404 Handler
+  app.use(notFound);
+
+  // Global Error Handler
+  app.use(errorHandler);
+
+  return app;
 }
 
-module.exports = express
+module.exports = express;
